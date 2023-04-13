6384f27ce9144513591ee7d2f196e199c9159774
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-04-13 19:29:35 -0300

    Initialize opentelemetry

---
+++ config/initializers/opentelemetry.rb
@@ -0,0 +1,8 @@
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
+require 'opentelemetry/instrumentation/all'
+
+OpenTelemetry::SDK.configure do |c|
+  c.service_name = 'dummy_web_app_otel'
+  c.use_all
+end
